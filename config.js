8d8d067fd7780ac5bf2e324f0002452cb2cd6cde
Katomineconnect <katomineconnect@gmail.com>
2025-07-12 18:20:00 +0600

    Create config.js

---
+++ config.js
@@ -0,0 +1,10 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyCeCNWpgo32Yr5wBKo_A2VW33Bc8PQXmV8",
+  authDomain: "katomineconnect-90087.firebaseapp.com",
+  databaseURL: "https://katomineconnect-90087-default-rtdb.firebaseio.com",
+  projectId: "katomineconnect-90087",
+  storageBucket: "katomineconnect-90087.firebasestorage.app",
+  messagingSenderId: "1070653902206",
+  appId: "1:1070653902206:web:a32d98f1bac28f8ebff72f",
+  measurementId: "G-H9GF2BQX34"
+};
